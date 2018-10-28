a658cb0daa917e64964d36c751092e766f2bfb07
Ronen Zolicha <ronen1245@gmail.com>
2018-10-28 11:12:21 +0200

    Revert "Restaurant search by location implementation"
    This reverts commit 45b63c32c4b14c3751bc835cf9e3011ff0258bd2.

--- RestIT/Models/ViewModels/RestaurantTypeViewModel.cs
+++ RestIT/Models/ViewModels/RestaurantTypeViewModel.cs
@@ -6,15 +6,11 @@ using System.Threading.Tasks;
 
 namespace RestIT.Models.ViewModels
 {
-    public class RestaurantSearchViewModel
+    public class RestaurantTypeViewModel
     {
         public List<Restaurant> Restaurants;
         public SelectList Types;
-        public SelectList Locations;
-        public SelectList Raitings;
         public string RestaurantType { get; set; }
-        public string RestaurantLocation { get; set; }
-        public int RestaurantRating { get; set; }
         public string SearchString { get; set; }
     }
 }
